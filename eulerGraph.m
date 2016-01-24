7764491c029809e1a87bcf025b6d1ed212db13d1
Tejas Niphadkar <tejas.niphadkar@gmail.com>
2016-01-24 14:43:09 +0530

    Final commit for First Reuqirement Set

--- eulerGraph.m
+++ eulerGraph.m
@@ -5,9 +5,11 @@
 %Create an object to the class by giving the Graph Matrix as an Argument.
 %Call startEuler function without any argument.
 
-classdef eulerGraph
+%To do: A diagraph with no. of edges more than 1 between two vertices.
+
+classdef eulerGraph < handle
     properties
-        graphInp, nVertices, oddVertex , nTotalEdgesTwice , eulerIncludedEdges, eulerAnsArray ;
+        graphInp, nVertices, oddVertex , nTotalEdgesTwice , eulerIncludedEdges, eulerAnsArray , visited ;
     end
     methods
         function obj = eulerGraph(edges)
@@ -21,7 +23,7 @@ classdef eulerGraph
             %Function Assumes that graph is connected.If not, a simple dfs will do.
             for i=1:1:obj.nVertices
                 sumRow = sum(obj.graphInp(i,:));
-                obj.nTotalEdgesTwice= obj.nTotalEdgesTwice +sumRow; 
+                obj.nTotalEdgesTwice= obj.nTotalEdgesTwice +sumRow;
                 if mod(sumRow,2)~=0
                     oddVertexCount = oddVertexCount+ 1;
                     obj.oddVertex = i ;
@@ -34,7 +36,7 @@ classdef eulerGraph
             end
         end
         function ansArray = startEuler(obj)
-            if ~isEuler()
+            if ~obj.isEuler()
                 fprintf('The Graph Matrix Input Does not have a valid Euler Path/Circuit');
             end
             obj.eulerIncludedEdges = 0;
@@ -45,25 +47,53 @@ classdef eulerGraph
             ansArray = obj.eulerAnsArray;
         end
         function storeEuler(obj,vertex) 
+            %fprintf('here at %d',vertex);
             for i=1:1:obj.nVertices
                 adj = obj.graphInp(vertex,i);
-                if adj~=-1 && isValidNextEdge(vertex,i)
+                if adj~=-1 && adj~=0 && obj.isValidNextEdge(vertex,i)
+                    %fprintf('at edge %d-%d',vertex,i);
                     obj.eulerIncludedEdges = obj.eulerIncludedEdges +1;
                     obj.eulerAnsArray(obj.eulerIncludedEdges,1) = vertex;
                     obj.eulerAnsArray(obj.eulerIncludedEdges,2) = i;
-                    obj.rmvEdge(u,i);
+                    obj.rmvEdge(vertex,i);
                     obj.storeEuler(i);
-                    break;
                 end
             end
         end
         function rmvEdge(obj,u,v)
             obj.graphInp(v,u) = -1;
-            obj.graphInp(u,v) = -1; %#ok<MCHV2>
+            obj.graphInp(u,v) = -1; 
         end
         function addEdge(obj,u,v)
             obj.graphInp(u,v) = 1;
             obj.graphInp(v,u) = 1;
+        end
+        function isValid= isValidNextEdge(obj,u,v)
+            %fprintf('at edge checking if valid %d-%d\n',u,v);
+            remVertices=0;
+            for i= 1:1:obj.nVertices
+                if obj.graphInp(u,i) ~= -1 && obj.graphInp(u,i)~=0
+                    remVertices = remVertices+ 1;
+                end
+            end
+            obj.visited = zeros(1,obj.nVertices);
+            countWithEdge = obj.dfsCount(u);
+            obj.rmvEdge(u,v);
+            obj.visited = zeros(1,obj.nVertices);
+            countWithoutEdge = obj.dfsCount(u);
+            obj.addEdge(u,v);
+            isValid = (remVertices==1 || countWithEdge==countWithoutEdge);
+        end
+        function countVisited = dfsCount(obj,u)
+            obj.visited(u)=1;
+            countVisited=1;
+            %fprintf('Visited %d\n',u);
+            for i= 1:1:obj.nVertices
+                if obj.graphInp(u,i)~=-1 && obj.graphInp(u,i)~=0 && ~obj.visited(i)
+                    countVisited = countVisited + obj.dfsCount(i);
+                    %fprintf('Increased count for %d->%d',u,i);
+                end
+            end
         end
     end
 end
